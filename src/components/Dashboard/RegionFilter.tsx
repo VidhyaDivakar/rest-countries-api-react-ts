b0c2a32ce91ccdba3c27bcb3bd2bd8faf164d715
Shanu Priya <priyads12@gmail.com>
2026-05-06 12:23:51 -0400

    Added filter by region functionality

--- src/components/Dashboard/RegionFilter.tsx
+++ src/components/Dashboard/RegionFilter.tsx
@@ -1,11 +1,15 @@
-function RegionFilter(){
+function RegionFilter({handleSearchFilter}){
 
     const regions = ["All", "Americas", "Asia", "Europe", "Oceania"]
+    const handleFilter=(e)=>{
+        console.log(e.target.value)
+        handleSearchFilter(e.target.value);
+    }
 
     return (<div>
         <span>Filter By Region</span>
         <div>
-            {regions.map(region=><button key={region} type="button">{region}</button>)}
+            {regions.map(region=><button key={region} type="button" value={region} onClick={handleFilter}>{region}</button>)}
         </div>
     </div>)
 }
